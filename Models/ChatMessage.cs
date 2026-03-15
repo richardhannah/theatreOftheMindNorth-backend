4910124fbdc41ceca46ca2f4dd8f78bb2a3ca6e1
richard hannah <richard.hannah1974@gmail.com>
2026-03-15 10:16:02 +0000

    update chatmessage model

--- Models/ChatMessage.cs
+++ Models/ChatMessage.cs
@@ -3,6 +3,7 @@ namespace TheatreOfTheMind.Models;
 public class ChatMessage
 {
     public string Name { get; set; } = "";
+    public string PlayerName { get; set; } = "";
     public string Text { get; set; } = "";
     public long Ts { get; set; }
     public bool IsDiceRoll { get; set; }
